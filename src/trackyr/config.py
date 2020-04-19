aa4c91f318b0865094e851f0506fd693c052745f
Fyre-Homelab <58180427+Fyre-Homelab@users.noreply.github.com>
2020-04-18 22:10:59 -0500

    Update config.py

--- src/trackyr/config.py
+++ src/trackyr/config.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-with open('/etc/trackyr/config.json') as config_file:
+with open('/etc/trackyr-config.json') as config_file:
     config = json.load(config_file)
 
 class Config:
