a7044be037085fe5229d55e132e47383fca0b07d
Fyre-Homelab <58180427+Fyre-Homelab@users.noreply.github.com>
2020-07-10 21:16:50 -0500

    Update routes.py

--- src/trackyr/notification_agents/routes.py
+++ src/trackyr/notification_agents/routes.py
@@ -1,63 +1,26 @@
-
-from flask import (Blueprint, abort, flash, redirect, render_template, request, url_for)
+from flask import (Blueprint, abort, flash, redirect, render_template, request, url_for, request)
 from trackyr import db
 from trackyr.models import NotificationAgent
 from trackyr.notification_agents.forms import NotificationAgentForm
 
+import lib.utils.logger as log
+
 import lib.core.notif_agent as notifAgent
 from lib.core.state import State
 
-
-
 notification_agents = Blueprint('notification_agents', __name__)
-
-@notification_agents.route("/notification_agents/test", methods=['POST'])
-def test_notification_agent():
-    json = request.json
-    webhook_url = json['webhook']
-    if json['username']:
-        username = json['username']
-    else:
-        username = "Trackyr"
-    Dict = {1: 'discord'}
-
-    test_notify = notifAgent.NotifAgent(module=Dict.get(int(json['module'])),
-                                        module_properties={'webhook': webhook_url, 'botname': username,
-                                                           'avatar': json['avatar']})
-
-    notifAgent.test_notif_agent(test_notify)
-    return "Success"
-
 
 @notification_agents.route("/notification_agents/create", methods=['GET', 'POST'])
 def create_notification_agents():
     State.load()
     form = NotificationAgentForm()
     if form.validate_on_submit():
-        if form.test.data:
-            # This button will send a test notification to the Notification Agent.
-            # Will need to add IF statements to send different notification messages to different agents.
-
-            webhook_url=form.webhook_url.data
-            if form.username.data:
-                username=form.username.data
-            else:
-                username="Trackyr"
-
-
-            Dict = {1: 'discord'}
-
-            test_notify = notifAgent.NotifAgent(module=Dict.get(form.module.data), module_properties={'webhook':webhook_url,'botname':username, 'avatar':form.icon.data})
-            notifAgent.test_notif_agent(test_notify)
-            
-            # Notification message on webui
-            flash("A test message has been sent to your notification agent", "notification")
-        else:
+        if form.submit.data:
             notification_agent = NotificationAgent(module=form.module.data,
-                                                    name=form.name.data,
-                                                    webhook_url=form.webhook_url.data,
-                                                    username=form.username.data,
-                                                    icon=form.icon.data)
+                                                   name=form.name.data,
+                                                   webhook_url=form.webhook_url.data,
+                                                   username=form.username.data,
+                                                   icon=form.icon.data)
             db.session.add(notification_agent)
             db.session.commit()
 
@@ -91,6 +54,18 @@ def edit_notification_agent(notification_agent_id):
         form.name.data = notification_agents.name
         form.webhook_url.data = notification_agents.webhook_url
         form.username.data = notification_agents.username
+        notification_agents.icon = form.icon.data
+        db.session.commit()
+
+        State.refresh_notif_agents()
+
+        flash('Your notification agent has been updated!', 'top_flash_success')
+        return redirect(url_for('main.notification_agents', notification_agent_id=notification_agents.id))
+    elif request.method == 'GET':
+        form.module.data = notification_agents.module
+        form.name.data = notification_agents.name
+        form.webhook_url.data = notification_agents.webhook_url
+        form.username.data = notification_agents.username
         form.icon.data = notification_agents.icon
         return render_template('create-notification-agent.html', title='Update Notification Agent',
                             form=form, legend='Update Notification Agent')
@@ -105,3 +80,23 @@ def delete_notification_agent(notification_agent_id):
 
     flash('Your notification agent has been deleted.', 'top_flash_success')
     return redirect(url_for('main.notification_agents'))
+
+@notification_agents.route("/notification_agents/test", methods=['POST'])
+def test_notification_agent():
+    form = NotificationAgentForm()
+    json = request.json
+    webhook_url = json['webhook']
+    if json['username']:
+        username = json['username']
+    else:
+        username = "Trackyr"
+    Dict = {1: 'discord'}
+
+    test_notify = notifAgent.NotifAgent(module=Dict.get(int(json['module'])),
+                                        module_properties={'webhook': webhook_url,
+                                                           'botname': username,
+                                                           'avatar': json['avatar']})
+
+    notifAgent.test_notif_agent(test_notify)
+
+    return "Success"
