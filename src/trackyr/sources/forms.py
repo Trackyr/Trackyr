c2167dd60557fe2e1e46496a185bff3a60827d9f
Fyre <fyre.homelab+gitkraken@gmail.com>
2020-07-29 21:29:05 -0500

    Dynamic source forms.py

--- src/trackyr/sources/forms.py
+++ src/trackyr/sources/forms.py
@@ -2,8 +2,22 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
+import lib.utils.logger as log
+import trackyr.models as models
+
 class SourceForm(FlaskForm):
-    MODULE_CHOICES = [(0,'Please Select a Module'), (1,'Kijiji')]
+    MODULE_CHOICES = [(0,'Please Select a Module')]
+
+    modules = Modules.query.all()
+
+    for m in modules:
+        if m.category == "source":
+            MODULE_CHOICES.append(m.id, m.name)
+
+    #MODULE_CHOICES = [(0,'Please Select a Module'), (1,'Kijiji')]
+
+    log.info_print(f"MODULE_CHOICES: {MODULE_CHOICES}")
+
     module = SelectField('Module', validators=[DataRequired()], coerce=int, choices=MODULE_CHOICES)
     name = StringField('Name', validators=[DataRequired()])
     website = StringField('Website')
