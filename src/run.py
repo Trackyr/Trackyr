25e4a174ef948354b935b0f5376db2cb7535e57b
Fyre <fyre.homelab+gitkraken@gmail.com>
2020-08-12 09:53:45 -0500

    [Test] moving source generation in the db.

--- src/run.py
+++ src/run.py
@@ -3,7 +3,6 @@ import lib.core.modules as modules
 
 app = create_app()
 
-modules.generate_sources_in_db()
-
 if __name__ == '__main__':
     app.run(debug=True)
+    modules.generate_sources_in_db()
